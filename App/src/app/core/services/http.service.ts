8d285206de4b9d13f6b0a20fdecb828f89917368
Oscar Geovany Rico <geo981202@gmail.com>
2026-02-28 08:30:06 -0500

    feat(dependencies): update Angular packages to version 21.2.0 and adjust TypeScript configuration

--- App/src/app/core/services/http.service.ts
+++ App/src/app/core/services/http.service.ts
@@ -1,3 +1,4 @@
+/// <reference types="@angular/common/types/http" />
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
